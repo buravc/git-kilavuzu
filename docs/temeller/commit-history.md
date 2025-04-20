c42dadcf0c3a32f897c8ac5964a5a08754b42230
buravc <avcii.brk@gmail.com>
2025-04-20 19:02:38 +0200

    refactor git-graf-motoru to commit-history

---
+++ docs/temeller/commit-history.md
@@ -0,0 +1,28 @@
+# Commit History
+
+Git repository'deki yapilan degisiklikleri gruplamak ve saklamak icin **commit** denilen bir yapi kullanmaktadir. Bir **commit** attigimizda aslinda o zamanda yaptigimiz degisiklikleri tek bir grup icerisinde toplayarak repository'e kayit etmis oluruz. Git ogrenirken yapilan en yaygin hata bence bir **commit**'in tam olarak ne oldugunu anlamamak ve varsayimlarla devam etmek. Internetteki kaynaklarin bircogu da buna dogru bir sekilde deginememekte.
+
+Bir **commit**, hem bir sahibi olan, yani bir kullanici adi ve email ile bagdastirilan, hem de bir **parent commit**'i olan bir yapidir. Yani her **commit**'in **en az bir parent commit**'i olmak zorundadir. Bunu `git log` komutunda `--parents` bayragini kullanarak gorebiliriz. Buna ek olarak ciktiyi sadelestirmek icin `--oneline` bayragini da ekleyelim.
+
+```bash
+$ git log --graph --parents --oneline
+* c973c9d 419a640 (HEAD -> yeni-branch) yeni-branch icin ilk commitimi atiyorum
+* 419a640 beddf6b (main) Dosyaya Merhaba Dunya ekledim
+* beddf6b Ilk commit
+```
+
+- `--oneline` bayragi ile artik log'da her satir bir commit'e denk geliyor ve hash'larin sadece ilk 7 karakteri yaziliyor.
+- `--parents` bayragi ile her satirin basinda 2 adet farkli hash bulunuyor. Ilk hash commit'in kendi hash'i iken ikinci hash ise parent commit'in hash'i oluyor.
+
+Log'da da gorulebilecegi uzere aslinda commit'lerimiz arasinda linked list'e benzer, geriye donuk bir graf yapisi bulunmaktadir. Commit'ler kendilerinden once gelen commit'leri, yani *parent commit*'i isaret ederek bir agac olustururlar.
+
+!!! note "Parent Commit"
+    `Parent` icin Turkce bir terim bulamadim. Burada bir onceki commit veya **ata commit** diye dusunebiliriz.
+!!! note "En az bir parent"
+    Gelecekte bazi commit'lerin birden cok parent'i olabildigini gorecegiz. Burada bir istisna olarak bir repository'de atilan ilk commit'i ornek gosterebiliriz. Ilk oldugu icin o commit'in bir parent'i bulunmamaktadir.
+
+Bu bilgilerin isiginda gectigimiz sayfada hazirladigimiz repository'i uzerinde yaptigimiz degisiklikler sonucunda sekillenen **commit history**'i su sekilde gorsellestirebiliriz.
+
+![image info](./images/commit-history-1.gif)
+
+Buradan yola cikarak, bir Branch'in **commit history**'de bir noktaya basitce isaret eden bir yapi oldugu sonucuna varabiliriz.
--- docs/temeller/images/git-frame.py
+++ docs/temeller/images/git-frame.py
@@ -26,43 +26,68 @@ class Commit():
         scene.add(self.circle, self.message)
         if self.parent is not None:
             scene.add(self.arrow)
-    def animate(self, scene: Scene):
-        scene.play(Create(self.circle), Create(self.message))
+    def animate(self, scene: Scene, *otherObjects: Mobject | Animation):
+        scene.play(Create(self.circle), Create(self.message), otherObjects)
         if self.parent is not None:
             scene.play(Create(self.arrow))
 
+class Ref():
+    commit: Commit = None
+    arrow: Arrow
+    text: Text
+    branchName: str
+    def __init__(self, branchName: str):
+        self.branchName = branchName
+    def get_animated_commit_change(self, commit: Commit, scene: Scene):
+        if self.commit is None:
+            self.commit = commit
+            start_point=[commit.circle.get_center()[0], -3,0]
+            direction=commit.circle.get_center() - start_point
+            unit_direction = direction / np.linalg.norm(direction)
+            end_point = commit.circle.get_center() - unit_direction * commit.circle.radius
+            self.text = Text(self.branchName,color=BLACK,font_size=25).move_to([start_point[0], start_point[1], 0])
+            self.arrow = Arrow(start=start_point, end=end_point, color=BLACK)
+            return Create(self.arrow), Create(self.text)
+        else:
+            self.commit = commit
+            start_point=[commit.circle.get_center()[0], self.arrow.get_center()[1],0]
+            return self.arrow.animate.move_to(start_point), self.text.animate.move_to([start_point[0], self.text.get_center()[1], 0])
 
 
 class GitCommitDiagram(Scene):
     def construct(self):
         self.camera.background_color = WHITE
-        commit1 = Commit("Ilk commit", LEFT*5)
-        commit2 = Commit("Dosyaya Merhaba\nDunya ekledim", 0, commit1)
-        commit3 = Commit("yeni-branch icin\nilk commitimi atiyorum", RIGHT*5, commit2)
+        commitMessages=["Ilk commit", "Dosyaya Merhaba\nDunya ekledim", "yeni-branch icin\nilk commitimi atiyorum"]
+        commit1 = Commit(commitMessages[0], LEFT*5)
+        commit2 = Commit(commitMessages[1], 0, commit1)
+        commit3 = Commit(commitMessages[2], RIGHT*5, commit2)
         
-        gitInitCommitText = self.create_shell_command_prompt("git commit -m \"Ilk commit\"")
-        gitFixBugCommitText = self.create_shell_command_prompt("git commit -m \"Dosyaya Merhaba\nDunya ekledim\"")
-        gitAddNewFeatureCommitText = self.create_shell_command_prompt("git commit -m \"yeni-branch icin\nilk commitimi atiyorum\"")
+        gitInitCommitText = self.create_shell_command_prompt(f"git commit -m \"{commitMessages[0]}\"")
+        gitFixBugCommitText = self.create_shell_command_prompt(f"git commit -m \"{commitMessages[1]}\"")
+        gitAddNewFeatureCommitText = self.create_shell_command_prompt(f"git commit -m \"{commitMessages[2]}\"")
+
+        mainBranchPointer = Ref("main")
         
-        self.spawn_commit(commit1, None, gitInitCommitText)
+        self.spawn_commit(commit1, None, gitInitCommitText, mainBranchPointer)
 
-        self.wait(3)
+        self.wait(2)
 
-        self.spawn_commit(commit2, gitInitCommitText, gitFixBugCommitText)
+        self.spawn_commit(commit2, gitInitCommitText, gitFixBugCommitText, mainBranchPointer)
 
-        self.wait(3)
+        self.wait(2)
 
-        self.spawn_commit(commit3, gitFixBugCommitText, gitAddNewFeatureCommitText)
+        yeniBranchPointer = Ref("yeni-branch")
+
+        self.spawn_commit(commit3, gitFixBugCommitText, gitAddNewFeatureCommitText, yeniBranchPointer)
         
-        self.wait(3)
+        self.wait(2)
 
-    def spawn_commit(self, commit: Commit, prevPrompt: Paragraph, newPrompt: Paragraph):
+    def spawn_commit(self, commit: Commit, prevPrompt: Paragraph, newPrompt: Paragraph, branchRef: Ref):
         if prevPrompt is not None:
             self.remove(prevPrompt)
         
         self.play(Create(newPrompt))
-        commit.animate(self)
-        
+        commit.animate(self, *branchRef.get_animated_commit_change(commit, self))
     
     def create_shell_command_prompt(self, msg: str):
         text = Paragraph("$ " + msg, alignment="center").scale_to_fit_width(6).move_to(UP*2)
